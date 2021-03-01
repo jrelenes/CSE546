89705befb9758050a62505f547329c73c95c2bd1
jrelenes <56177974+jrelenes@users.noreply.github.com>
2021-03-01 20:06:36 +0000

    Add files via upload

--- CSE_546.py
+++ CSE_546.py
@@ -5,20 +5,30 @@ from botocore.exceptions import ClientError
 import queue_wrapper
 logger = logging.getLogger(__name__)
 sqs = boto3.resource('sqs')
+QUEUE_SIZE = 0
 #image
 #png <->bites, add separate atribute for hash id
-def send_message_image(queue, data):
+def send_image(queue, data):
+    global QUEUE_SIZE
     response = queue.send_message(
             MessageBody=data)
+    QUEUE_SIZE+=1
 
 #receives images and text
-def receive_messages(queue):
+def get_image(queue):
+    global QUEUE_SIZE
     for message in queue.receive_messages():
         # Print out the body of the message
-        print(message.body)
+        #print(message.body)
 
         # Let the queue know that the message is processed
         message.delete()
+        QUEUE_SIZE-=1
+
+def get_size(queue):
+	global QUEUE_SIZE
+	return QUEUE_SIZE
+
 
 #str('x10\x06\x00\x00\x00\xfd')
 def main():
@@ -27,10 +37,11 @@ def main():
     image_data = bytearray()
     with open("test.png", "rb") as fp:
         image_data = fp.read()
-
-    print(image_data)
-    send_message_image(queue, str(image_data))
-    receive_messages(queue)
+    print(get_size(queue))
+    send_image(queue, str(image_data))
+    print(get_size(queue))
+    get_image(queue)
+    print(get_size(queue))
 
 if __name__ == '__main__':
     main()
