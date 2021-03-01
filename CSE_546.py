2251d3f494de70d8b2fb3d4528664a90d1a95a76
jrelenes <56177974+jrelenes@users.noreply.github.com>
2021-03-01 16:55:53 +0000

    Add files via upload

--- CSE_546.py
+++ CSE_546.py
@@ -1,59 +1,39 @@
 import logging
 import sys
-
 import boto3
 from botocore.exceptions import ClientError
-
 import queue_wrapper
-
 logger = logging.getLogger(__name__)
 sqs = boto3.resource('sqs')
-
-
 #image
 #png <->bites, add separate atribute for hash id
-def send_message(queue, image, hash_id, message_attributes=None):
+#def send_message(queue, image, hash_id, message_attributes=None):
 
-	
-    response = queue.send_message(
-            Image=image,
-            Hash_Id=hash_id)
-            
-    return response
-    
+
+#    response = queue.send_message(
+#            Image=image,
+#            Hash_Id=hash_id)
+
 #text
-def send_message(queue, message_body, message_attributes=None):
-
-    
-    if not message_attributes:
-        message_attributes = {}
-
-
+def send_message(queue, hash_id):
     response = queue.send_message(
-            MessageBody=message_body,
-            MessageAttributes=message_attributes)
-            
-    return response
+            MessageBody=hash_id)
 
+#receives images and text
+def receive_messages(queue):
+    for message in queue.receive_messages():
+        # Print out the body of the message
+        print('{0}'.format(message.body))
 
+        # Let the queue know that the message is processed
+        message.delete()
 
-def receive_messages(queue, max_number, wait_time):
-	
-
-    images = queue.receive_messages(
-            MessageAttributeNames=['All'],
-            MaxNumberOfMessages=max_number,
-            WaitTimeSeconds=wait_time
-        )
-    for index in images:
-            #need to alter to images format in message labeled as message_id and body
-            logger.info("Received message: %s: %s", msg.message_id, msg.body)
-    
-    #returns images
-    return images
-
-
+#str('x10\x06\x00\x00\x00\xfd')
 def main():
+    queue = queue_wrapper.create_queue('sqs-usage-demo-message-wrapper')
+    hash_id = "b'\\x89PNG\\r\\n\\x1a\\n\\x00\\x00\\x00\\rIHDR\\x00\\x00\\x00\\xc8\\x00\\x00\\x00\\xc8\\x10\\x06\\x00\\x00\\x00\\xfd'"
+    send_message(queue, hash_id)
+    receive_messages(queue)
 
 if __name__ == '__main__':
     main()
