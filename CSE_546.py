e678613892bf9ec8b7cad4a95fd704d9ccee8415
jrelenes <56177974+jrelenes@users.noreply.github.com>
2021-03-01 18:18:24 +0000

    Add files via upload

--- CSE_546.py
+++ CSE_546.py
@@ -7,23 +7,15 @@ logger = logging.getLogger(__name__)
 sqs = boto3.resource('sqs')
 #image
 #png <->bites, add separate atribute for hash id
-#def send_message(queue, image, hash_id, message_attributes=None):
-
-
-#    response = queue.send_message(
-#            Image=image,
-#            Hash_Id=hash_id)
-
-#text
-def send_message(queue, hash_id):
+def send_message_image(queue, data):
     response = queue.send_message(
-            MessageBody=hash_id)
+            MessageBody=data)
 
 #receives images and text
 def receive_messages(queue):
     for message in queue.receive_messages():
         # Print out the body of the message
-        print('{0}'.format(message.body))
+        print(message.body)
 
         # Let the queue know that the message is processed
         message.delete()
@@ -31,8 +23,13 @@ def receive_messages(queue):
 #str('x10\x06\x00\x00\x00\xfd')
 def main():
     queue = queue_wrapper.create_queue('sqs-usage-demo-message-wrapper')
-    hash_id = "b'\\x89PNG\\r\\n\\x1a\\n\\x00\\x00\\x00\\rIHDR\\x00\\x00\\x00\\xc8\\x00\\x00\\x00\\xc8\\x10\\x06\\x00\\x00\\x00\\xfd'"
-    send_message(queue, hash_id)
+    #hash_id = "b'\\x89PNG\\r\\n\\x1a\\n\\x00\\x00\\x00\\rIHDR\\x00\\x00\\x00\\xc8\\x00\\x00\\x00\\xc8\\x10\\x06\\x00\\x00\\x00\\xfd'"
+    image_data = bytearray()
+    with open("test.png", "rb") as fp:
+        image_data = fp.read()
+
+    print(image_data)
+    send_message_image(queue, str(image_data))
     receive_messages(queue)
 
 if __name__ == '__main__':
